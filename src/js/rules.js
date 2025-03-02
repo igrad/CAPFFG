35027a5fbb1c074fb2dd74c82663b27ffb45db0f
Ian Gradert <iangradert@gmail.com>
2025-03-01 22:14:50 -0600

    wip, alphabetize

--- src/js/rules.js
+++ src/js/rules.js
@@ -363,141 +363,6 @@ const RuleAlignConsecutiveBitFields = {
   ]
 }
 
-const RuleAlignDeclarations = {
-  version: 3.8,
-  type: null,
-  title: 'Align Consecutive Declarations',
-  desc: 'Style of aligning consecutive declarations.',
-  note: '',
-  subRules: [
-    {
-      title: 'Enabled',
-      desc: 'Whether aligning is enabled.',
-      codeSnippet:
-        '#define SHORT_NAME       42\n' +
-        '#define LONGER_NAME      0x007F\n' +
-        '#define EVEN_LONGER_NAME (2)\n' +
-        '#define foo(x)           (x * x)\n' +
-        '#define bar(y, z)        (y + z)\n' +
-        '\n' +
-        'int a            = 1;\n' +
-        'int somelongname = 2;\n' +
-        'double c         = 3;\n' +
-        '\n' +
-        'int aaaa : 1;\n' +
-        'int b    : 12;\n' +
-        'int ccc  : 8;\n' +
-        '\n' +
-        'int         aaaa = 12;\n' +
-        'float       b = 23;\n' +
-        'std::string ccc;',
-    },
-    {
-      type: typeof (bool),
-      title: 'AcrossEmptyLines',
-      desc: 'Whether to align across empty lines',
-      codeSnippet:
-        'true:\n' +
-        'int a = 1\n' +
-        'int somelongname = 2;\n' +
-        'double c         = 3;\n' +
-        '\n' +
-        'int d            = 3;\n' +
-        '\n' +
-        'false:\n' +
-        'int a            = 1;\n' +
-        'int somelongbane = 2;\n' +
-        'double c         = 3;\n' +
-        '\n' +
-        'int d = 3;\n'
-    },
-    {
-      type: typeof (bool),
-      title: 'AcrossComments',
-      desc: 'Whether to align across comments',
-      codeSnippet:
-        'true:\n' +
-        'int d    = 3;\n' +
-        '/* A comment. */\n' +
-        'double e = 4;\n' +
-        '\n' +
-        'false:\n' +
-        'int d = 3;\n' +
-        '/* A comment. */\n' +
-        'double e = 4;\n'
-    },
-    {
-      type: typeof (bool),
-      title: 'AlignCompound',
-      desc: 'Only for `AlignConsecutiveAssignments`. Whether compound ' +
-        'assignments like `+=` are aligned along with `=`.',
-      codeSnippet:
-        'true:\n' +
-        'a   &= 2;\n' +
-        'bbb  = 2;\n' +
-        '\n' +
-        'false:\n' +
-        'a &= 2;\n' +
-        'bbb = 2;'
-    },
-    {
-      type: typeof (bool),
-      title: 'AlignFunctionDeclarations',
-      desc: 'Only for `AlignConsecutiveDeclarations`. Whether function ' +
-        'declarations are aligned.',
-      codeSnippet:
-        'true:\n' +
-        'unsigned int f1(void);\n' +
-        'void         f2(void);\n' +
-        'size_t       f3(void);\n' +
-        '\n' +
-        'false:\n' +
-        'unsigned int f1(void);\n' +
-        'void f2(void);\n' +
-        'size_t f3(void);'
-    },
-    {
-      type: typeof (bool),
-      title: 'AlignFunctionPointers',
-      desc: 'Only for `AlignConsecutiveDeclarations`. Whether function ' +
-        'pointers are aligned.',
-      codeSnippet:
-        'true:\n' +
-        'unsigned i;\n' +
-        'int     &r;\n' +
-        'int     *p;\n' +
-        'int      (*f)();\n' +
-        '\n' +
-        'false:\n' +
-        'unsigned i;\n' +
-        'int     &r;\n' +
-        'int     *p;\n' +
-        'int (*f)();'
-    },
-    {
-      type: typeof (bool),
-      title: 'PadOperators',
-      desc: 'Only for `AlignConsecutiveAssignments`. Whether short ' +
-        'assignment operators are left-padded to the same length as long ' +
-        'ones in order to put all assignment operators to the right of the ' +
-        'left hand side.',
-      codeSnippet:
-        'true:\n' +
-        'a   >>= 2;\n' +
-        'bbb   = 2;\n' +
-        '\n' +
-        'a     = 2;\n' +
-        'bbb >>= 2;\n' +
-        '\n' +
-        'false:\n' +
-        'a >>= 2;\n' +
-        'bbb = 2;\n' +
-        '\n' +
-        'a     = 2;\n' +
-        'bbb >>= 2;'
-    }
-  ]
-}
 
 const RuleAlignConsecutiveMacros = {
   version: 9,
@@ -736,6 +601,238 @@ const RuleAlignConsecutiveShortCaseStatements = {
         'default:           return "";\n' +
         '}',
     }
+  ]
+}
+
+const RuleAlignDeclarations = {
+  version: 3.8,
+  type: null,
+  title: 'Align Consecutive Declarations',
+  desc: 'Style of aligning consecutive declarations.',
+  note: '',
+  subRules: [
+    {
+      title: 'Enabled',
+      desc: 'Whether aligning is enabled.',
+      codeSnippet:
+        '#define SHORT_NAME       42\n' +
+        '#define LONGER_NAME      0x007F\n' +
+        '#define EVEN_LONGER_NAME (2)\n' +
+        '#define foo(x)           (x * x)\n' +
+        '#define bar(y, z)        (y + z)\n' +
+        '\n' +
+        'int a            = 1;\n' +
+        'int somelongname = 2;\n' +
+        'double c         = 3;\n' +
+        '\n' +
+        'int aaaa : 1;\n' +
+        'int b    : 12;\n' +
+        'int ccc  : 8;\n' +
+        '\n' +
+        'int         aaaa = 12;\n' +
+        'float       b = 23;\n' +
+        'std::string ccc;',
+    },
+    {
+      type: typeof (bool),
+      title: 'AcrossEmptyLines',
+      desc: 'Whether to align across empty lines',
+      codeSnippet:
+        'true:\n' +
+        'int a = 1\n' +
+        'int somelongname = 2;\n' +
+        'double c         = 3;\n' +
+        '\n' +
+        'int d            = 3;\n' +
+        '\n' +
+        'false:\n' +
+        'int a            = 1;\n' +
+        'int somelongbane = 2;\n' +
+        'double c         = 3;\n' +
+        '\n' +
+        'int d = 3;\n'
+    },
+    {
+      type: typeof (bool),
+      title: 'AcrossComments',
+      desc: 'Whether to align across comments',
+      codeSnippet:
+        'true:\n' +
+        'int d    = 3;\n' +
+        '/* A comment. */\n' +
+        'double e = 4;\n' +
+        '\n' +
+        'false:\n' +
+        'int d = 3;\n' +
+        '/* A comment. */\n' +
+        'double e = 4;\n'
+    },
+    {
+      type: typeof (bool),
+      title: 'AlignCompound',
+      desc: 'Only for `AlignConsecutiveAssignments`. Whether compound ' +
+        'assignments like `+=` are aligned along with `=`.',
+      codeSnippet:
+        'true:\n' +
+        'a   &= 2;\n' +
+        'bbb  = 2;\n' +
+        '\n' +
+        'false:\n' +
+        'a &= 2;\n' +
+        'bbb = 2;'
+    },
+    {
+      type: typeof (bool),
+      title: 'AlignFunctionDeclarations',
+      desc: 'Only for `AlignConsecutiveDeclarations`. Whether function ' +
+        'declarations are aligned.',
+      codeSnippet:
+        'true:\n' +
+        'unsigned int f1(void);\n' +
+        'void         f2(void);\n' +
+        'size_t       f3(void);\n' +
+        '\n' +
+        'false:\n' +
+        'unsigned int f1(void);\n' +
+        'void f2(void);\n' +
+        'size_t f3(void);'
+    },
+    {
+      type: typeof (bool),
+      title: 'AlignFunctionPointers',
+      desc: 'Only for `AlignConsecutiveDeclarations`. Whether function ' +
+        'pointers are aligned.',
+      codeSnippet:
+        'true:\n' +
+        'unsigned i;\n' +
+        'int     &r;\n' +
+        'int     *p;\n' +
+        'int      (*f)();\n' +
+        '\n' +
+        'false:\n' +
+        'unsigned i;\n' +
+        'int     &r;\n' +
+        'int     *p;\n' +
+        'int (*f)();'
+    },
+    {
+      type: typeof (bool),
+      title: 'PadOperators',
+      desc: 'Only for `AlignConsecutiveAssignments`. Whether short ' +
+        'assignment operators are left-padded to the same length as long ' +
+        'ones in order to put all assignment operators to the right of the ' +
+        'left hand side.',
+      codeSnippet:
+        'true:\n' +
+        'a   >>= 2;\n' +
+        'bbb   = 2;\n' +
+        '\n' +
+        'a     = 2;\n' +
+        'bbb >>= 2;\n' +
+        '\n' +
+        'false:\n' +
+        'a >>= 2;\n' +
+        'bbb = 2;\n' +
+        '\n' +
+        'a     = 2;\n' +
+        'bbb >>= 2;'
+    }
+  ]
+}
+
+const RuleAlignEscapedNewlines = {
+  version: 5,
+  title: 'Align Escaped Newlines',
+  desc: 'Options for aligning backslashes in escaped newlines.',
+  options: [
+    {
+      title: 'ENAS_DontAlign',
+      config: 'DontAlign',
+      desc: 'Don\'t align escaped newlines.',
+      codeSnippet:
+        '#define A \\\n' +
+        '  int aaaa; \\\n' +
+        '  int b; \\\n' +
+        '  int ddddddddddd;'
+    },
+    {
+      title: 'ENAS_Left',
+      config: 'Left',
+      desc: 'Align escaped newlines as far left as possible.',
+      codeSnippet:
+        '#define A   \\\n' +
+        '  int aaaa; \\\n' +
+        '  int b;    \\\n' +
+        '  int ddddddddddd;'
+    },
+    {
+      title: 'ENAS_LeftWithLastLine',
+      config: 'LeftWithLastLine',
+      desc: 'Align escaped newlines as far left as possible, using the last ' +
+        'line of the preprocessor directive as the reference if it\n the ' +
+        'longest.',
+      codeSnippet:
+        '#define A          \\\n' +
+        '  int aaaa;        \\\n' +
+        '  int b;           \\\n' +
+        '  int ddddddddddd;'
+    },
+    {
+      title: 'ENAS_Right',
+      config: 'Right',
+      desc: 'Align escaped newlines in the right-most column.',
+      codeSnippet:
+        '#define A                                           \\\n' +
+        '  int aaaa;                                         \\\n' +
+        '  int b;                                            \\\n' +
+        '  int ddddddddddd;'
+    }
+  ]
+}
+
+const RuleAlignOperands = {
+  version: 3.5,
+  title: 'Align Operands',
+  desc: 'If `true`, horizontally align operands of binary and ternary ' +
+    'expressions.',
+  options: [
+    {
+      title: 'OAS_DontAlign',
+      config: 'DontAlign',
+      desc: 'Do not align operands of binary and ternary expressions. The ' +
+        'wrapped lines are indented `ContinuationIndentWidth` spaces from ' +
+        'the start of the line.',
+      codeSnippet:
+        'int aaa = bbbbbbbbbbbbbbb +\n' +
+        '    ccccccccccccccc;'
+    },
+    {
+      title: 'OAS_Align',
+      config: 'Align',
+      desc: 'Horizontally align operands of binary and ternary ' +
+        'expressions.\n' +
+        'Specifically, this aligns operands of a single expression that ' +
+        'to be split over multiple lines. When ' +
+        '`BreakBeforeBinaryOperators` is set, the wrapped operator is ' +
+        'aligned with the operand on the first line.',
+      codeSnippet:
+        'int aaa = bbbbbbbbbbbbbbb +\n' +
+        '          ccccccccccccccc;\n' +
+        '/* Or with BreakBeforeBinaryOperators... */\n' +
+        'int aaa = bbbbbbbbbbbbbbb\n' +
+        '          + ccccccccccccccc;\n',
+    },
+    {
+      title: 'OAS_AlignAfterOperator',
+      config: 'AlignAfterOperator',
+      desc: 'This is similar to `OAS_Align`, except when ' +
+        '`BreakBeforeBinaryOperators` is set, the operator is un-indented ' +
+        'so that the wrapped operand is aligned with the operand on the ' +
+        'first line.',
+      codeSnippet:
+        'int aaa = bbbbbbbbbbbbbbb\n' +
+        '        + ccccccccccccccc;\n',
+    },
   ]
 }
 
@@ -1172,102 +1269,6 @@ const RuleAlignTableGenDefinitionColons = {
   ]
 }
 
-const RuleAlignEscapedNewlines = {
-  version: 5,
-  title: 'Align Escaped Newlines',
-  desc: 'Options for aligning backslashes in escaped newlines.',
-  options: [
-    {
-      title: 'ENAS_DontAlign',
-      config: 'DontAlign',
-      desc: 'Don\'t align escaped newlines.',
-      codeSnippet:
-        '#define A \\\n' +
-        '  int aaaa; \\\n' +
-        '  int b; \\\n' +
-        '  int ddddddddddd;'
-    },
-    {
-      title: 'ENAS_Left',
-      config: 'Left',
-      desc: 'Align escaped newlines as far left as possible.',
-      codeSnippet:
-        '#define A   \\\n' +
-        '  int aaaa; \\\n' +
-        '  int b;    \\\n' +
-        '  int ddddddddddd;'
-    },
-    {
-      title: 'ENAS_LeftWithLastLine',
-      config: 'LeftWithLastLine',
-      desc: 'Align escaped newlines as far left as possible, using the last ' +
-        'line of the preprocessor directive as the reference if it\n the ' +
-        'longest.',
-      codeSnippet:
-        '#define A          \\\n' +
-        '  int aaaa;        \\\n' +
-        '  int b;           \\\n' +
-        '  int ddddddddddd;'
-    },
-    {
-      title: 'ENAS_Right',
-      config: 'Right',
-      desc: 'Align escaped newlines in the right-most column.',
-      codeSnippet:
-        '#define A                                           \\\n' +
-        '  int aaaa;                                         \\\n' +
-        '  int b;                                            \\\n' +
-        '  int ddddddddddd;'
-    }
-  ]
-}
-
-const RuleAlignOperands = {
-  version: 3.5,
-  title: 'Align Operands',
-  desc: 'If `true`, horizontally align operands of binary and ternary ' +
-    'expressions.',
-  options: [
-    {
-      title: 'OAS_DontAlign',
-      config: 'DontAlign',
-      desc: 'Do not align operands of binary and ternary expressions. The ' +
-        'wrapped lines are indented `ContinuationIndentWidth` spaces from ' +
-        'the start of the line.',
-      codeSnippet:
-        'int aaa = bbbbbbbbbbbbbbb +\n' +
-        '    ccccccccccccccc;'
-    },
-    {
-      title: 'OAS_Align',
-      config: 'Align',
-      desc: 'Horizontally align operands of binary and ternary ' +
-        'expressions.\n' +
-        'Specifically, this aligns operands of a single expression that ' +
-        'to be split over multiple lines. When ' +
-        '`BreakBeforeBinaryOperators` is set, the wrapped operator is ' +
-        'aligned with the operand on the first line.',
-      codeSnippet:
-        'int aaa = bbbbbbbbbbbbbbb +\n' +
-        '          ccccccccccccccc;\n' +
-        '/* Or with BreakBeforeBinaryOperators... */\n' +
-        'int aaa = bbbbbbbbbbbbbbb\n' +
-        '          + ccccccccccccccc;\n',
-    },
-    {
-      title: 'OAS_AlignAfterOperator',
-      config: 'AlignAfterOperator',
-      desc: 'This is similar to `OAS_Align`, except when ' +
-        '`BreakBeforeBinaryOperators` is set, the operator is un-indented ' +
-        'so that the wrapped operand is aligned with the operand on the ' +
-        'first line.',
-      codeSnippet:
-        'int aaa = bbbbbbbbbbbbbbb\n' +
-        '        + ccccccccccccccc;\n',
-    },
-  ]
-}
-
 const RuleAlignTrailingComments = {
   version: 3.7,
   title: 'Align Trailing Comments',
@@ -1375,6 +1376,108 @@ const RuleAllowAllConstructorInitializersOnNextLine = {
 }
 
 const RuleAllowAllParametersOfDeclarationOnNextLine = {
+  version: 3.3,
+  type: 'bool',
+  title: 'Allow All Arguments On Next Line',
+  desc: 'If a function call or braced initializer list doesn\'t fit on a ' +
+    'line, allow putting all arguments onto the next line, even if ' +
+    '`BinPackArguments` is `false`.',
+  codeSnippet:
+    'true:\n' +
+    'callFunction(\n' +
+    '    a, b, c, d);\n' +
+    '\n' +
+    'false:\n' +
+    'callFunction(a,\n' +
+    '             b,\n' +
+    '             c,\n' +
+    '             d);\n'
+}
+
+const RuleAllowBreakBeforeNoexceptSpecifier = {
+  version: 18,
+  title: 'Allow Break Before Noexcept Specifier',
+  desc: 'Controls if there could be a line break before a `noexcept` ' +
+    'specifier.',
+  options: [
+    {
+      title: 'BBNSS_Never',
+      config: 'Never',
+      desc: 'No line break allowed.',
+      codeSnippet:
+        'void foo(int arg1,\n' +
+        '         double arg2) noexcept;\n\n' +
+        'void bar(int arg1, double arg2) noexcept(\n' +
+        '    noexcept(baz(arg1)) &&\n' +
+        '    noexcept(bas(arg2)));',
+    },
+    {
+      title: 'BBNSS_OnlyWithParen',
+      config: 'OnlyWithParen',
+      desc: 'For a simple `noexcept` there is no line break allowed, but \n' +
+        'when we have a condition is.',
+      codeSnippet:
+        'void foo(int arg1,\n' +
+        '         double arg2) noexcept;\n\n' +
+        'void bar(int arg1, double arg2)\n' +
+        '    noexcept(noexcept(baz(arg1)) &&\n' +
+        '             noexcept(bas(arg2)));',
+    },
+    {
+      title: 'BBNSS_Always',
+      config: 'Always',
+      desc: 'Line breaks are allowed, but note that because of the ' +
+        'associated penalties `clang-format` often prefers not to ' +
+        'break before the `noexcept`.',
+      codeSnippet:
+        'void foo(int arg1,\n' +
+        '         double arg2) noexcept;\n\n' +
+        'void bar(int arg1, double arg2)\n' +
+        '    noexcept(noexcept(baz(arg1)) &&\n' +
+        '             noexcept(bas(arg2)));',
+    },
+  ]
+}
+
+const RuleAllowShortCaseExpressionOnASingleLine = {
+  version: 3.5,
+  title: 'Allow Short Blocks on a Single Line',
+  desc: 'Dependent on the value, `while (true) { continue; }` can be \n' +
+    'put on a single line.',
+  options: [
+    {
+      title: 'SBS_Never',
+      config: 'Never',
+      desc: 'Never merge blocks into a single line.',
+      codeSnippet:
+        'while (true) {\n' +
+        '}\n' +
+        'while (true) {\n' +
+        '  continue;' +
+        '}\n',
+    },
+    {
+      title: 'SBS_Empty',
+      config: 'Empty',
+      desc: 'Only merge empty blocks.',
+      codeSnippet:
+        'while (true) {}\n' +
+        'while (true) {\n' +
+        '  continue;' +
+        '}\n',
+    },
+    {
+      title: 'SBS_Always',
+      config: 'Always',
+      desc: 'Always merge short blocks into a single line.',
+      codeSnippet:
+        'while (true) {}\n' +
+        'while (true) { continue; }',
+    },
+  ]
+}
+
+const RuleAllowShortCaseLabelsOnASingleLine = {
   version: 3.3,
   type: 'bool',
   title: 'Allow All Arguments On Next Line',
